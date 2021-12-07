7965b92082d40a1a42ef3cd0eb40ccad30ddb435
Lukáš Píšek <kostici.lopisek@gmail.com>
2021-12-07 17:53:11 +0100

    Add project files.

---
+++ DayOne/Program.cs
@@ -0,0 +1,10 @@
+﻿float res = 0;
+int[] input = Array.ConvertAll(File.ReadAllText("Input.txt").Split("\n"), int.Parse);
+for (int i = 0; i < input.Length - 1; i++)
+{
+    if (input[i + 1] > input[i])
+    {
+        res++;
+    }
+}
+Console.WriteLine(res);

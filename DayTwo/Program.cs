38f89ad7e25716ea10c6d4de7339fbbb47517d7c
Lukáš Píšek <kostici.lopisek@gmail.com>
2021-12-07 22:55:27 +0100

    Day Two Part One

---
+++ DayTwo/Program.cs
@@ -0,0 +1,19 @@
+﻿float hor = 0;
+float vert = 0;
+string[] input = File.ReadAllText("Input.txt").Split("\n");
+foreach (string i in input)
+{
+    if (i.Contains("forward"))
+    {
+        hor += Convert.ToInt32(i.Remove(0, 8));
+    }
+    else if(i.Contains("up"))
+    {
+        vert -= Convert.ToInt32(i.Remove(0, 3));
+    }
+    else if(i.Contains("down"))
+    {
+        vert += Convert.ToInt32(i.Remove(0, 5));
+    }
+}
+Console.WriteLine(hor * vert);

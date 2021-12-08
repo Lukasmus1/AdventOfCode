8535b4518d8882b794d2118b3f25672dbff84b8e
Lukáš Píšek <kostici.lopisek@gmail.com>
2021-12-08 11:24:21 +0100

    Day 3 part one

---
+++ Day3/Program.cs
@@ -0,0 +1,26 @@
+﻿string gamma = "";
+string epsilon = "";
+string[] input = File.ReadAllText("Input.txt").Split("\n");
+for (int l = 0; l < input[l].Length - 1; l++)
+{
+    int zero = 0;
+    int one = 0;
+    foreach (string i in input)
+    {
+        if (i[l] == '1')
+            one++;
+        else
+            zero++;
+    }
+    if (zero > one)
+    {
+        gamma += "0";
+        epsilon += "1";
+    }
+    else
+    {
+        gamma += "1";
+        epsilon += "0";
+    }
+}
+Console.WriteLine(Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2));
